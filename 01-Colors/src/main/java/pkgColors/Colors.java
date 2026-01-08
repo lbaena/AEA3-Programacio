16830e077c2f9bc35c9ccc66a45c068c3e9ca193
Lluc Baena Llansola <llucbaenallansola@gmail.com>
2026-01-08 17:17:13 +0100

    quit statics

--- 01-Colors/src/main/java/pkgColors/Colors.java
+++ 01-Colors/src/main/java/pkgColors/Colors.java
@@ -5,14 +5,14 @@ public class Colors {
     public static final String RESET = "\u001B[0m";
 
     // Colores de texto estándar
-    public static final String NEGRO = "\u001B[30m";
-    public static final String ROJO = "\u001B[31m";
-    public static final String VERDE = "\u001B[32m";
-    public static final String AMARILLO = "\u001B[33m";
-    public static final String AZUL = "\u001B[34m";
-    public static final String PURPURA = "\u001B[35m";
-    public static final String CIAN = "\u001B[36m";
-    public static final String BLANCO = "\u001B[37m";
+    public final String NEGRO = "\u001B[30m";
+    public final String ROJO = "\u001B[31m";
+    public final String VERDE = "\u001B[32m";
+    public final String AMARILLO = "\u001B[33m";
+    public final String AZUL = "\u001B[34m";
+    public final String PURPURA = "\u001B[35m";
+    public final String CIAN = "\u001B[36m";
+    public final String BLANCO = "\u001B[37m";
 
     // Colores de fondo (Background)
     public static final String FONDO_ROJO = "\u001B[41m";
